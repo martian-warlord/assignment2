1a5fac239b80be7f27552d844cfffd8857db5f78
Joshua Darlington <joshua_darlington@yahoo.com>
2014-10-08 09:08:43 -0500

    initial slop bucket setup of assignment one for harvard responsive web design class

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
 
 			<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
 			<script src="js/respond.js"></script>
-			<link href='http://fonts.googleapis.com/css?family=Lato%7CCherry+Cream+Soda%7CCookie' rel='stylesheet' type='text/css'>
+
+			<link href='http://fonts.googleapis.com/css?family=Carrois+Gothic+SC' rel='stylesheet' type='text/css'>
 			<link href="css/style.css" rel="stylesheet" type="text/css" />
+
 
 </head>
 
@@ -18,100 +20,311 @@
 
 
 <body>
+      <nav class="navbar navbar-default  navbar-top" role="navigation">
+   
+
+   <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+  <p class="navbar-text navbar-left"><a href="#" class="navbar-link glyphicon glyphicon-align-justify"></a></p>
+  </div>
+</nav>
+
+<div class="jumbotron">
+<div class="container">
+        <p>Joshua Darlington</p>
+        <p>XD</p>
+        
+      </div>
+    </div>
+
+<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title ">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          <span class="glyphicon glyphicon-user btn-circle  btn-lg"></span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+
+
+
+       <div class="jumbotron">
 <div class="container">
 
 
+    <section class="col-md-5 col-sm-12">
+<img  alt="Joshua Darlington Headshot" src="Images/Joshua-Darlington-Headshot.jpg" />
 
-<header>
-<div class="row">
-		<div  class="col-md-12 col-sm-12" >
-		<h1>Joshua Darlington</h1> 
-		<h4>Dynamic Web Apps Portfolio</h4>
-		</div>
-</div>
-
-<div class="row">
-		<div  class="col-md-6 col-sm-6" >
-		<img  alt="Joshua Darlington Headshot" src="Images/Joshua-Darlington-Headshot.jpg" />
-		</div>
-
-
-		<div  class="col-md-4 col-sm-4" >
-		<h2>P2</h2>
-		<ul><li>View: <a href="http://p2.joshuainmadison.me">[link]</a> </li>
-		<li>Github: <a href="https://github.com/martian-warlord/p2">[link]</a></li>
-		</ul>
-		</div>
-
-		<div  class="col-md-4 col-sm-4" >
-
-		<h2>P3</h2>
-		<ul>
-		<li>View: [link] </li>
-		<li>Github: [link]</li>
-		</ul>
-		</div>
-
-
-		<div  class="col-md-4 col-sm-4" >
-		<h2>P4</h2>
-		<ul><li>View: [link] </li>
-		<li>Github: [link]</li>
-		</ul>
-		</div>
-</div>
-
-</header>
-
-<article>
-<div class="row">
-<div  class="col-md-6 col-sm-6" >
-
-		<h3>Who are you?</h3>
-		<p>I am in the middle of a career change.  </p>
-
-		<p>I was a professional musician who worked a variety of day jobs in the Los Angeles entertainment industry.  I recently moved to the midwest with my wife and child, and my skillset is not a good match for the Madison job market.  
+ 
+    </section>
+    <div class="col-md-1 col-sm-0">
+<p>|<br></p>
 </div>
 
 
-<div  class="col-md-6 col-sm-6" >
+    <section class="col-md-6 col-sm-12">
+    <!-- tab panel2>
+    <!- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="active"><a href="#star" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-star btn-circle  btn-lg"></span></a></li>
+      <li><a href="#tree" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-tree-deciduous btn-circle  btn-lg"></span></a></li>
+      <li><a href="#heart" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-heart btn-circle  btn-lg"></span></a></li>
+    </ul>
 
-		<h3>Why DWA?</h3>
-		<p>I would like to improve my LAMP skills.  </p>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div class="tab-pane active" id="star"><h2>I am</h2> <p>a former rock star with 20 years of success navigating the hyper competitive landscape of 
+        music and entertainment.  </p> <p>A digital artist, designer and producer.  
+
+           <ul>
+    <li>20+ years of experience using Adobe software tools.</li>
+    <li>Formally trained @ Interface design, graphic design, and computational design </li>
+    <li>A Certified agile project manager </li>
+    </ul>
+        </p>
+
+
+        </div>
+      <div class="tab-pane" id="tree"><h2>I am</h2> <p>A husband and a father.  </p><p>I recently moved to Madison to follow my wife who is 
+        now the managing editor of Update magazine at the Wisconsin School of Business.    </p></div>
+      <div class="tab-pane" id="heart"><h2>I am</h2>
+
+<p>Passionate about design and making beautiful and emotional experiences.  
+<ul>
+    <li>User centric</li>
+    <li>Tech agnostic</li>
+    <li>A functionality first design fundamentalist</li></ul>
+    
+    
+
+<p>Inspired by: Social Psychology, Social Cognition, Society of Mind, Neuroscience and Behavior, Behavioral Endocrinology, Irrationality, 
+   Evolution, Ecology and Behavior, Environmental Psychology, Animal Behavior, Evolution of Brain and Language, Heidegger’s Being and Time, 
+   Mathematics: Making the Invisible Visible, Model Thinking, Philosophy of Language, and Theory of Meaning.</p>
+
+
+
+      </div>
+    </div>
+    </section>
+    </row>
+
+<!-- jumbo container closing -->
+</div></div>
+
+
+
+
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title  g">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                    <span class="glyphicon glyphicon-eye-open btn-circle  btn-lg"></span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+
+
+       <div class="jumbotron">
+<div class="container">
+
+
+  <section class="col-md-5 col-sm-12">
+    <h2>#UXD </h2>
+    <ul>
+    <li>20+ years of Creative Leadership</li>
+    <li>Defining & Driving Design process</li>
+    <li>Digital Kung Fu Master</li>
+    </ul>
+    <p>Paragraph of ad copy</p>
+ 
+    </section>
+    <div class="col-md-1 col-sm-0">
+<p>|</p>
 </div>
+
+
+    <section class="col-md-6 col-sm-12">
+    <!-- tab panel2>
+    <!- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="active"><a href="#design" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-eye-open btn-circle  btn-lg"></span></a></li>
+      <li><a href="#film" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-film btn-circle  btn-lg"></span></a></li>
+      <li><a href="#audio" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-headphones btn-circle  btn-lg"></span></a></li>
+      <li><a href="#writing" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-pencil btn-circle  btn-lg"></span></a></li>
+      <li><a href="#code" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-cog btn-circle  btn-lg"></span></a></li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div class="tab-pane active" id="design">...</div>
+      <div class="tab-pane" id="film">...</div>
+      <div class="tab-pane" id="audio">
+        <p>20+ years of experience as an independent audio designer, composer and producer<p>
+<ul><li>Game audio for independent developers</li>
+
+<li>Composer for APM music library. Licensees include PBS and A&E</li>
+
+<li>AAA Digital audio engineer, sound designer, and team lead at Stargate Studios</li>
+
+<li>Audio engineer and sound designer at NYU TSOA graduate recording studios</li>
+    </ul>
+
+<p>Education includes a two year apprentice to composer and sound designer Mars Lasar.</p>
+
+<p>Undergraduate Coursework at NYU in MIDI production, directing vocal performance, 
+location sound recording, and audio post production, and sound as art.  Graduate coursework at NYU ITP in audio computation. </p>
+
+
+
+
+
+      </div>
+      <div class="tab-pane" id="writing"><p>Over 20 years of experience as a transmedia narrative designer</p>
+<ul><li>Game writing consultant at Focal Press</li>
+
+<li>Narrative Designer on Bello Galactica mobile game</li>
+
+<li>TV Writer on Dip Shitz pilot for Palm d'Or Productions</li>
+
+<li>Contributing writer to Gamasutra, Ultraculture, and Tablet Magazine </li>
+
+<li>Development coordinator KGM LA</li>
+
+<li>Producer at Micromind software startup</li>
+
+<li>Producer at Tasty Productions</li>
+
+<li>Assistant producer and office manager at A*Vision, Atlantic Records</li>
+
+<li>Assistant story coordinator at Paramount Television/Hardcopy</li>
+
+<li>Assistant producer at LSL Productions</li>
+
+<li>Reader and development assistant at Double Play Productions</li></ul></div>
+
+
+
+
+
+
+
+
+
+      <div class="tab-pane" id="code">
+<ul><li>I am presently working towards a Master’s Degree in Digital Media and Instructional Design and a Graduate Certificate 
+in Web Technologies from Harvard’s Extension Program.</li>
+
+<li>Previous graduate coursework at New York University’s Interactive Telecommunications Program includes computational media, 
+  interface design, and electrical engineering. </li></ul>
+  <p>I like to dabble in C languages like C/C++, Java, PHP, Python, Ruby, PHP, Javascript, and Actionscript.</p>
+  <p>Past experience includes hand coding (HTML, Javascript, Actionscript) over a dozen media rich music web sites to market my work as a professional recording artist.</P>
+    
+
+
+
+      </div>
+    </div>
+    </section>
+    </row>
+
+<!-- jumbo container closing -->
+</div></div>
+
+
+</div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title ">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <span class="glyphicon glyphicon-barcode btn-circle  btn-lg"></span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+
+<!--JUMBO -->        
+       <div class="jumbotron">
+<div class="container">
+
+
+  <section class="col-md-5 col-sm-12">
+    <h2>BRACKETED HEADLINE</h2>
+    <ul>
+    <li>bulleted list</li>
+    <li>list item two</li>
+    <li>#3rule of threes</li>
+    </ul>
+    <p>Paragraph of ad copy</p>
+ 
+    </section>
+    <div class="col-md-1 col-sm-0">
+<p>|<br>|<br>|<br>|<br>|<br>|<br>|<br></p>
 </div>
 
+    <section class="col-md-6 col-sm-12">
+    <!-- tab panel2>
+    <!- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="active"><a href="#biz1" role="tab" data-toggle="tab">Experience</a></li>
+      <li><a href="#biz2" role="tab" data-toggle="tab">Education</a></li>
+      <li><a href="#t23" role="tab" data-toggle="tab">Messages</a></li>
+    </ul>
 
-<div class="row">
-<div  class="col-md-6 col-sm-6" >
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div class="tab-pane active" id="biz1"><ul>
+<li>Creative producer (CAPM, CSM) with over twenty years of success leading creative talent on agile productions 
+while coordinating the complex flow of technically detailed time critical elements across multiple boundaries.</li>
 
-		<h3>Code Experience?</h3>
+<li>Content creation, content production and content administration.</li>
 
-		<p>I did two semesters of Hypercard/Computational Media at New York University's Interactive Telecommunication Program.  </p>
-
-		<p>I've hand coded HTML and Javascript for about a dozen web 1.0 music web sites and I've dabbled in various C languages such as C/C++, Ruby, PHP, Java, Javascript, Actionscript, Python, and Perl.</p>
-</div>
-
-
-<div  class="col-md-6 col-sm-6" >
-
-		<h3>Mac or PC?</h3>
-
-		<p>I am in the process of transitioning from an old PC to a new Mac.</p>
-
-</div>
-</div>
-
-<div class="row">
-<div  class="col-md-6 col-sm-6" >
-
-		<h3>Anything else?</h3>
-		<p>I am interested in the creative uses of data and would appreciate any pointers in that direction. </p> 
-		</div>
-		</div>
+<li>Corporate and independent clients including Sony, Toyota, MTV, ABC, EMI, PBS, A&E, Sega, Avon, Canadian Food Network, 
+  Rocket Science, APM, Stargate Studios, Ken Gross Management, Quantum Tracks, Qwerty Prods., Focal Press, House of Blues, 
+  Doom Inc., Wordsound Recordings, Gamasutra, Tablet Magazine, Palm d'Or Social Club, Karl Records, Ultraculture, Sniderware, 
+  Bello Galactica, Helper Games and GK Photography. </li></ul>
 
 
-</article>
+
+      </div>
+      <div class="tab-pane" id="biz2"><ul>
+<li>Graduate certificate in Producing from UCLA’s Professional Program.  Certified in project management (CaPM) from PMI.  
+  Certified in agile project management (CSM) methodology.</li>
+
+<li>Graduate coursework at New York University’s Interactive Telecommunications Program in consumer marketing.  
+  Undergraduate education at NYU and University of Michigan in TV & radio station Management , film production, 
+  communications law and economics. </li>
+
+<li>MOOC coursework includes content management, gamification, technology entrepreneurship, RFID supply chain management, lean Sigma 6, 
+  and finance.
+</li></ul>
+
+
+      </div>
+      <div class="tab-pane" id="t23">...</div>
+    </div>
+    </section>
+    </row>
+
+<!-- jumbo container closing -->
+</div></div>
+
+      </div>
+    </div>
+  </div>
+
+
+  
 
 <!-- close container -->
 </div>
