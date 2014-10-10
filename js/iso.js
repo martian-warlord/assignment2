6dfd8abd75ec066bd24a8ba5d5698f7611175eb1
Joshua Darlington <joshua_darlington@yahoo.com>
2014-10-10 15:42:18 -0500

    final tweeaks

---
+++ js/iso.js
@@ -0,0 +1,10 @@
+$.getScript('http://cdn.jsdelivr.net/isotope/1.5.25/jquery.isotope.min.js',function(){
+
+  /* activate jquery isotope */
+  $('#posts').imagesLoaded( function(){
+    $('#posts').isotope({
+      itemSelector : '.item'
+    });
+  });
+  
+});
